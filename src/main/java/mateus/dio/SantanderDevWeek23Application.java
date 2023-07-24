cc4e471dfcf6055e8ddae187cf4685d69f118ffb
Hakisho <117363193+Hakisho@users.noreply.github.com>
2023-07-24 18:14:53 -0300

    Initial commit

---
+++ src/main/java/mateus/dio/SantanderDevWeek23Application.java
@@ -0,0 +1,13 @@
+package mateus.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevWeek23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWeek23Application.class, args);
+	}
+
+}
---
+++ src/test/java/mateus/dio/SantanderDevWeek23ApplicationTests.java
@@ -0,0 +1,13 @@
+package mateus.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeek23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
